260317e61a5fc34609a5b9039836ff8a4f002874
Zoe R <zoe.rivera.1@uni.massey.ac.nz>
2016-04-10 20:33:07 +1200

    Edited app.js to separate pinned and recent tweets

--- Project/Xeromatic/Content/app.js
+++ Project/Xeromatic/Content/app.js
@@ -9,29 +9,46 @@ var Tweet = React.createClass({
 var App = React.createClass({
 	//React function that sets the initial state of the app (where changeable data is stored)
 	getInitialState: function() {
-		return {tweets: []};
+	    return {
+	        recentTweets: [],
+            pinnedTweets: []
+	    };
 	},
 
 	//React function that runs after the app first loads
 	componentDidMount: function() {
-		var self = this;
-		fetch('/recentTweets', {method: 'get'})
-			.then(function(response) {
+		var self = this; // 'self' refers to the whole component
+		var recentFetch = fetch('/recentTweets', {method: 'get'})
+			.then(function(response) { // This function turns response into json data
 				return response.json();
 			})
-			.then(function(data) {
-				self.setState({tweets: data});
+
+		var pinnedFetch = fetch('/pinnedTweets', {method: 'get'})
+			.then(function(response) { // This function turns response into json data
+			    return response.json();
+			})
+
+        Promise.all([recentFetch, pinnedFetch]) // Wait for everything in these brackets to return before doing anything with them
+			.then(function(data) { // This function takes the result of the previous 'then' above 
+			    self.setState({recentTweets: data[0], pinnedTweets: data[1]});
 			})
 			.catch(function(error) {
-				console.error('Error', error);
+			    console.error('Error', error);
 			});
 	},
 
 	//React function that runs on first load and whenever the state is changed
 	render: function() {
-		var tweets = (this.state.tweets.length > 0) ? this.state.tweets.map(function(tweet) {
-			return <Tweet key={tweet.Id} text={tweet.Text} />
+		var pinnedTweets = (this.state.pinnedTweets.length > 0) ? this.state.pinnedTweets.map(function(tweet) {
+		    // 'map' is a javascript array function
+		    return <Tweet key={tweet.Id} text={tweet.Text} />
 			})
+			: null;
+
+		var recentTweets = (this.state.recentTweets.length > 0) ? this.state.recentTweets.map(function(tweet) {
+		    // 'map' is a javascript array function
+		    return <Tweet key={tweet.Id} text={tweet.Text} />
+		    })
 			: null;
 
 		return (
@@ -41,7 +58,13 @@ var App = React.createClass({
 					<div className="panel-heading">
 						<h3 className="panel-title">Pinned Tweets</h3>
 					</div>
-					<ul className="list-group">{tweets}</ul>
+					<ul className="list-group">{pinnedTweets}</ul>
+				</div>
+                <div className="panel panel-default">
+					<div className="panel-heading">
+						<h3 className="panel-title">Recent Tweets</h3>
+					</div>
+					<ul className="list-group">{recentTweets}</ul>
 				</div>
 			</div>
 		);
